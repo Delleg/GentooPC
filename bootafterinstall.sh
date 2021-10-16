9bdfc2f9602dc574a994b86137966e1fceaf22e9
Artur Orysz <arturorysz@gmail.com>
2021-10-16 07:38:39 +0000

    first commit

--- bootafterinstall.sh
+++ bootafterinstall.sh
@@ -1,1 +1,19 @@
-
+mkdir /mnt/gentoo
+swapon /dev/nvme0n1p2
+mount /dev/nvme0n1p3 /mnt/gentoo
+mkdir /mnt/gentoo/boot
+mount /dev/nvme0n1p1 /mnt/gentoo/boot
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/dev
+test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
+mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
+chmod 1777 /dev/shm
+mkdir /mnt/gentoo/hostrun
+mount --bind /run /mnt/gentoo/hostrun/
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) $PS1"
