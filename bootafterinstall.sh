f7df707dc89d6c5430befcdce02ebd66a06f298a
Artur Orysz <arturorysz@gmail.com>
2022-09-19 17:38:42 +0200

    Update bootafterinstall.sh
    Change for new configuration

--- bootafterinstall.sh
+++ bootafterinstall.sh
@@ -1,10 +1,7 @@
 
-sudo su
-mkdir /mnt/{gentoo,backup}
-swapon /dev/nvme0n1p2
-mount /dev/nvme0n1p3 /mnt/gentoo
-mount /dev/nvme0n1p1 /mnt/gentoo/boot
-mount /dev/nvme0n1p4 /mnt/gentoo/backup
+swapon /dev/sda2
+mount /dev/sda3 /mnt/gentoo
+mount /dev/sda1 /mnt/gentoo/boot
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
@@ -14,8 +11,6 @@ mount --make-rslave /mnt/gentoo/dev
 test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
 mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
 chmod 1777 /dev/shm
-mkdir /mnt/gentoo/hostrun
-mount --bind /run /mnt/gentoo/hostrun/
 chroot /mnt/gentoo /bin/bash
 source /etc/profile
 export PS1="(chroot) $PS1"
